a7be142ae5d6f4e7ffee7135b994ebbd8425180d
jferrentino <jackie@Jackies-MacBook-Pro.local>
2015-12-13 17:42:43 -0500

    x axis scrollbar fixed needs backgoround image changed

--- js/main.js
+++ js/main.js
@@ -16,7 +16,9 @@ img.onload = function() {
     // jump to bottom
     window.scrollTo(0,document.body.scrollHeight);
 }
+
 img.src = "https://dl.dropboxusercontent.com./s/s5pt5ufz4roeoxa/IMAGETEST.png?dl=0";
+// img.src = "https://dl.dropboxusercontent.com./s/s5pt5ufz4roeoxa/IMAGETEST.png?dl=0";
 
 
 $('.myLinkToBot').click(function () {
@@ -35,7 +37,7 @@ $('.myMenuLink').click(function () {
 
 $('.gif-show--elf').on({
     mouseenter: function () {
-        
+
         // $(this).css({background-color: black});
          $(this).css("opacity", "1");
 

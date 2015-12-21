a0d60d1444511d9943576e659fc99a2b6ca61681
jferrentino <jackie@jackies-mbp.fios-router.home>
2015-12-21 00:19:44 -0500

    added girl deleted extraneous comments

--- js/main.js
+++ js/main.js
@@ -1,25 +1,6 @@
 (function(){
-// setTimeout(function() {
-//     window.scrollTo(0,document.body.scrollHeight);
-// }, 5000);
-
-// var img = new Image();
-// window.onload = function() {
-//     // when this has loaded
-//     // add iamge
-//     $('.fullscreen__image--building').attr('src', img.src);
-//     // show image
-//     $('.fullscreen__image--container').removeClass('fullscreen__hidden');
-//     // hide spinner
-//     $('.loader').addClass('fullscreen__hidden');
-//     // jump to bottom
-//     window.scrollTo(0,document.body.scrollHeight);
-// }
 
 window.scrollTo(0,document.body.scrollHeight);
-
-//img.src = "https://dl.dropboxusercontent.com./s/s5pt5ufz4roeoxa/IMAGETEST.png?dl=0";
-// img.src = "https://dl.dropboxusercontent.com./s/s5pt5ufz4roeoxa/IMAGETEST.png?dl=0";
 
 
 $('.myLinkToBot').click(function () {
@@ -37,18 +18,7 @@ $('.myMenuLink').click(function () {
     return false;
 });
 
-// $('.gif-show--elf').on({
-//     mouseenter: function () {
 
-//         // $(this).css({background-color: black});
-//          $(this).css("opacity", "1");
-
-//     },
-
-//     mouseleave: function () {
-//          $(this).css("opacity", "0");
-//     }
-// });
 
 $('.gif-show--guyWiggle').on({
     mouseenter: function () {
